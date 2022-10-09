7027f4fdf8120c7492f46b064c411ef3d9b58c76
haileytt51976 <115385295+haileytt51976@users.noreply.github.com>
2022-10-08 23:33:35 -0500

    Add files via upload
    lexical scoping assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+##for this assignment, I will be creating a couple of functions "makeCacheMatrix" and "cacheSolve"
 
-## Write a short comment describing this function
+##According to the assignment, "makeCache" is a "function that creates a special "matrix" object that can cache its inverse."
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x=matrix()){
+  p <-NULL
+  set<-function(y){
+    x<<-y
+    p<-NULL
+  }
+  get <-function()x
+  setInverse <- function(inverse)p<<-inverse
+  getInverse <- function()j
+  list(set =set, get=get,setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##According to the assignment, "cacheSolve" is a "function that computes the inverse of the special "matrix" returned by makeCacheMatrix"
+cacheSolve <- function(x, ...){
+  p<-x$getInverse()
+  if(!is.null(p)){
+    message("get cached result")
+    return(p)
+    mat<-x$get()
+    p<-solve(mat,...)
+    x$setInverse(p)
+    p
+  }
 }
